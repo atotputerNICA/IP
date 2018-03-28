79686da5370f55cbe5dfc4e0c136482b938626e0
pemil <p.emilia13@yahoo.com>
2018-03-28 16:27:53 +0300

    test-2

--- app/src/main/java/com/example/pemil/www/MainActivity.java
+++ app/src/main/java/com/example/pemil/www/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("TEST", "test de commit");
+        Log.d("TEST", "test de commit 2");
     }
 }
