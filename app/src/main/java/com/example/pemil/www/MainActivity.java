67730197c3aac66c75ac558b8a0a3f69a22f3648
atotputerNICA <nicadanamaria@gmail.com>
2018-04-28 18:09:25 +0300

    Dana

--- app/src/main/java/com/example/pemil/www/MainActivity.java
+++ app/src/main/java/com/example/pemil/www/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("TEST", "test de commit Ionut");
+        Log.d("TEST", "test de commit Dana");
     }
 }
