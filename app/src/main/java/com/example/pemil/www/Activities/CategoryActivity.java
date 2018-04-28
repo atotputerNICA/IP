e97bd041bcc79a0fb44af3302180136a98403001
pemil <p.emilia13@yahoo.com>
2018-04-28 21:30:40 +0300

    Added layouts for user profile and category

--- app/src/main/java/com/example/pemil/www/Activities/CategoryActivity.java
+++ app/src/main/java/com/example/pemil/www/Activities/CategoryActivity.java
@@ -1,16 +1,14 @@
 package com.example.pemil.www.Activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.pemil.www.R;
 
-public class MainActivity extends AppCompatActivity {
-
+public class CategoryActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.category);
     }
 }
--- app/src/main/java/com/example/pemil/www/Activities/UserProfileActivity.java
+++ app/src/main/java/com/example/pemil/www/Activities/UserProfileActivity.java
@@ -1,16 +1,18 @@
 package com.example.pemil.www.Activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.pemil.www.R;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by pemil on 28.04.2018.
+ */
 
+public class UserProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
