930475bc49e8fecff3665081d225c8dd42a0bccf
atotputerNICA <nicadanamaria@gmail.com>
2018-05-05 12:59:28 +0300

    Finished ScoreActivity

--- app/src/main/java/com/example/pemil/www/Activities/ScoreActivity.java
+++ app/src/main/java/com/example/pemil/www/Activities/ScoreActivity.java
@@ -1,53 +1,46 @@
 package com.example.pemil.www.Activities;
 
-import android.content.Context;
-import android.content.SharedPreferences;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.pemil.www.R;
 
-/**
- * @author atotputerNICA
- */
 public class ScoreActivity extends AppCompatActivity {
-    TextView a1, a2, a3, a4, a5, a6, a7, a8, a9, a10;
+    TextView correct, incorrect, attempted, score, you;
+    int cor = 0, incorr = 0, attempt = 0, scor = 0, yo = 0;
+    int x = 0;
+    private static final int def = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.results);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        SharedPreferences sharedPreferences = getSharedPreferences("Score", Context.MODE_PRIVATE);
-        a1 = (TextView) findViewById(R.id.computer);
-        a2 = (TextView) findViewById(R.id.sports);
-        a3 = (TextView) findViewById(R.id.inventions);
-        a4 = (TextView) findViewById(R.id.general);
-        a5 = (TextView) findViewById(R.id.science);
-        a6 = (TextView) findViewById(R.id.english);
-        a7 = (TextView) findViewById(R.id.books);
-        a8 = (TextView) findViewById(R.id.maths);
-        a9 = (TextView) findViewById(R.id.capitals);
-        a10 = (TextView) findViewById(R.id.currency);
-        try {
-            a1.setText("" + sharedPreferences.getInt("Computer", 0));
-            a2.setText("" + sharedPreferences.getInt("Sports", 0));
-            a3.setText("" + sharedPreferences.getInt("Inventions", 0));
-            a4.setText("" + sharedPreferences.getInt("General", 0));
-            a5.setText("" + sharedPreferences.getInt("Science", 0));
-            a6.setText("" + sharedPreferences.getInt("English", 0));
-            a7.setText("" + sharedPreferences.getInt("Books", 0));
-            a8.setText("" + sharedPreferences.getInt("Maths", 0));
-            a9.setText("" + sharedPreferences.getInt("Capitals", 0));
-            a10.setText("" + sharedPreferences.getInt("Currency", 0));
-        } catch (Exception e) {
-            Toast.makeText(ScoreActivity.this, "" + e, Toast.LENGTH_SHORT).show();
-        }
-    }
+        setContentView(R.layout.activity_result);
 
+        Intent intent = getIntent();
+        cor = intent.getIntExtra("correct", 0);
+        attempt = intent.getIntExtra("attemp", 0);
+        incorr = attempt - cor;
+        scor = 10 * cor;
+        correct = (TextView) findViewById(R.id.correct);
+        incorrect = (TextView) findViewById(R.id.incorrect);
+        attempted = (TextView) findViewById(R.id.attempted);
+        score = (TextView) findViewById(R.id.score);
+        you = (TextView) findViewById(R.id.you);
+
+        attempted.setText("  " + attempt);
+        correct.setText("  " + cor);
+        incorrect.setText("  " + incorr);
+        score.setText("Score  :    " + scor);
+        float x1 = (cor * 100) / attempt;
+        if (x1 < 40)
+            you.setText("You Need Improvement");
+        else if (x1 < 70)
+            you.setText("You are an average Quizzer");
+        else if (x1 < 90)
+            you.setText("You are an above average Quizzer ");
+        else
+            you.setText("You are a brilliant  Quizzer ");
+    }
 }
